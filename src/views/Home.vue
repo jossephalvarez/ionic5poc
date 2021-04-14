ed0c99344d594d4da1f4cff553fdf46d5032b8da
Josseph <jossephalvarez@gmail.com>
2021-04-14 14:14:41 +0200

    feat(): AndroidFingerprintAuth on Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,8 @@
         <ion-label>Josseph Alvarez</ion-label>
       </ion-item>
       <ion-item>
-        <ion-button color="secondary" expand="full" @click="openWebView()">Webview</ion-button>
-        <ion-button color="success" expand="full">Biometric</ion-button>
+        <ion-button color="primary" expand="full" @click="openWebView()">Webview</ion-button>
+        <ion-button color="tertiary" expand="full" @click="openBiometric()">Biometric</ion-button>
         <ion-button color="secondary" expand="full" @click="openModal()">Open Modal</ion-button>
         </ion-item>
     </ion-content>
@@ -25,6 +25,7 @@
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonAvatar, IonButton , modalController } from '@ionic/vue';
 import Modal from '../components/Modal';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth';
 
 import { defineComponent } from "vue";
 
@@ -32,7 +33,7 @@ export default defineComponent({
   name: 'Tab1',
   components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonAvatar, IonItem, IonLabel, IonButton },
   setup() {
-    const openModal = async () => {
+    const openModal = async (title) => {
       // Otherwise, make the call:
       try {
         const modal = await modalController
@@ -40,7 +41,7 @@ export default defineComponent({
                   component: Modal,
                   cssClass: 'my-custom-class',
                   componentProps: {
-                    title: 'Modal'
+                    title: title || 'Modal'
                   },
                 })
         return modal.present();
@@ -50,10 +51,39 @@ export default defineComponent({
     };
     const openWebView =() =>{
       InAppBrowser.create('https://ionicframework.com/','_blank', 'location=yes');
+    };
+    const openBiometric = () => {
+      AndroidFingerprintAuth.isAvailable()
+              .then((result)=> {
+                if(result.isAvailable){
+                  // it is available
+                  AndroidFingerprintAuth.encrypt({ clientId: 'myAppName', username: 'myUsername', password: 'myPassword' })
+                      .then(result => {
+                            if (result.withFingerprint) {
+                              console.log('Successfully encrypted credentials.');
+                              console.log('Encrypted credentials: ' + result.token);
+                              openModal('Open Modal WITH FINGER PRINT');
+                            } else if (result.withBackup) {
+                              console.log('Successfully authenticated with backup password!');
+                              openModal('Open Modal WITH BACKUP');
+                            } else console.log('Didn\'t authenticate!');
+                          })
+                      .catch(error => {
+                            if (error === AndroidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED) {
+                              console.log('Fingerprint authentication cancelled');
+                            } else console.error(error)
+                          });
+
+                } else {
+                  // fingerprint auth isn't available
+                }
+              })
+              .catch(error => console.error(error));
     }
     return {
       openModal,
-      openWebView
+      openWebView,
+      openBiometric
     };
   },
 
