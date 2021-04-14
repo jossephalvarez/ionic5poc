8ba695a88b8070002f2035f308047f6d34cf49ec
Josseph <jossephalvarez@gmail.com>
2021-04-14 10:47:19 +0200

    feat(): clean code

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -6,17 +6,6 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-<!--      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ExploreContainer name="Home Screen Tab" />
-
-      <ion-button color="primary" expand="full">Cam</ion-button>
-      <ion-button color="secondary" expand="full">Webview</ion-button>
-      <ion-button color="tertiary" expand="full">Location</ion-button>
-      <ion-button color="success" expand="full">Biometric</ion-button>-->
       <ion-item>
         <ion-avatar slot="start">
           <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y">
