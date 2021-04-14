2f2a862cdbd193a1a614172a1e66f8ba7f6d227c
Josseph <jossephalvarez@gmail.com>
2021-04-14 11:40:23 +0200

    feat(): modal added

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -13,19 +13,44 @@
         <ion-label>Item Avatar</ion-label>
       </ion-item>
       <ion-item>
-            <ion-button color="secondary" expand="full">Webview</ion-button>
-            <ion-button color="success" expand="full">Biometric</ion-button>
+        <ion-button color="secondary" expand="full" @click="openModal()">Webview</ion-button>
+        <ion-button color="success" expand="full">Biometric</ion-button>
+        <ion-button color="secondary" expand="full" @click="openModal()">Open Modal</ion-button>
         </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonAvatar } from '@ionic/vue';
-// import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonAvatar, IonButton , modalController } from '@ionic/vue';
+import Modal from '../components/Modal';
+// import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { defineComponent } from "vue";
 
-export default  {
+export default defineComponent({
   name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonAvatar, IonItem, IonLabel }
-}
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonAvatar, IonItem, IonLabel, IonButton },
+  setup() {
+    const openModal = async () => {
+      // Otherwise, make the call:
+      try {
+        const modal = await modalController
+                .create({
+                  component: Modal,
+                  cssClass: 'my-custom-class',
+                  componentProps: {
+                    title: 'Modal'
+                  },
+                })
+        return modal.present();
+      } catch (e) {
+        console.log("error", e);
+      }
+    };
+    return {
+      openModal
+    };
+  },
+
+})
 </script>
